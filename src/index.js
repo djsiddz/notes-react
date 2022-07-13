397b1dcbca8934155034a55a1b15d8517e7e49ce
Siddhesh Thadeshwar <djsiddz@gmail.com>
2022-07-13 19:49:23 +0530

    Add service worker and update manifest.json to make the app a PWA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker";
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
